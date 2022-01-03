39d9be0cfbfe3cf40978a2134ab5f9198b26b937
Nikolay Yakimov <96826786+Yakimov-Nikolay@users.noreply.github.com>
2022-01-03 17:01:07 +0200

    Add files via upload

---
+++ pathfinder/web/UserControllerTest.java
@@ -0,0 +1,23 @@
+package com.example.pathfinder.web;
+
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testOpenRegisterForm(){
+    }
+
+}
